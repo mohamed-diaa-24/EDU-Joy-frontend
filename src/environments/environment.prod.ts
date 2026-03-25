f9b3f27f1148713fb0b8e6591dad0b958840d6e4
mohamed diaa <mohamed.diaa.dev@gmail.com>
2026-03-25 21:12:58 +0200

    (feat) : add environment setting for production

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://localhost:5001/api',
+  stripePublishableKey:
+    'pk_test_51TEVVOAWTobSUm0PfmjE9lhtcqiqNDgNHFOWT9Ofi6io83fGsc2mqrdMoM37PBVFIpWQWdHtVowvtNV5pItdWJvr00XnGzYySI',
+};
